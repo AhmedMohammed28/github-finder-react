b3fa68717ad7fdbe3b503c25cca5eed2c6df0e6d
Ahmed Mohammed <ahmed.mohammed3588@gmail.com>
2023-01-03 19:38:58 +0200

    Alert Component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
+import Alert from "./components/layout/Alert";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/Alert/AlertContext";
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col h-screen justify-between">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-              <Route path="/notfound" element={<NotFound />}></Route>
-              <Route path="/*" element={<NotFound />}></Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col h-screen justify-between">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/notfound" element={<NotFound />}></Route>
+                <Route path="/*" element={<NotFound />}></Route>
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
